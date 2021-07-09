70647e0db8aed3b0345a38f63077f491f1e55598
WalukaUI <cwaluka@yahoo.com>
2021-07-09 00:15:39 -0700

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+# Your code goes here
+class Anagram
+    attr_accessor :name
+    def initialize(listen)
+       @name=listen
+    end
+    def match(list)
+            list.select { |word| word.split("").sort == @name.split("").sort }
+    end
+end
+
